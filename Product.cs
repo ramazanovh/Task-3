6cc4025168e3532288fac02d286be34e3a46d5a1
ramazanovh <150849536+ramazanovh@users.noreply.github.com>
2024-01-09 00:20:14 -0800

    Add files via upload

---
+++ Product.cs
@@ -0,0 +1,17 @@
+﻿namespace Product
+{
+    public class Product
+    {
+        private int Id { get; }
+        public string? Brand { get; set; }
+        public string? Name { get; set; }
+        public decimal Price { get; set; }
+        public Product(int id,string? brand, string? name, decimal price)
+        {
+            Id = id;
+            Brand = brand;
+            Name = name;
+            Price = price;
+        }
+    }
+}
